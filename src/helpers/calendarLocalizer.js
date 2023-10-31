485d6096007f716502e0386cd8b3f4979d33b8c1
unknown <enzzo2410@gmail.com>
2023-10-30 22:52:53 -0300

    Se arma la estuctura del calendario y se almacenan los eventos en el localstorage

---
+++ src/helpers/calendarLocalizer.js
@@ -0,0 +1,17 @@
+import { dateFnsLocalizer } from "react-big-calendar";
+import { getDay,startOfWeek,parse,format } from "date-fns";
+import esES from "date-fns/locale/es";
+
+
+
+const locales = {
+    "es": esES,
+};
+  
+export const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { CalendarApp } from './CalendarApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <CalendarApp />
-  </React.StrictMode>,
+  // </React.StrictMode>, 
 )
